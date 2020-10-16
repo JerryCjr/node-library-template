d63843814881660a5293cc40e9cf20213ffb3196
Jerry <jerryc_js@163.com>
2020-10-16 15:48:42 +0800

    [DEMO] 测试axios, express, path, lodash

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,11 @@
 import path from 'path';
+import { chunk } from 'lodash';
+import axios from 'axios';
+import app from 'express';
 
-console.log(__dirname);
-console.log(path.resolve(__dirname, 'src'));
+console.log(path);
+console.log(chunk);
+console.log(axios);
+console.log(app);
 
-export {};
+export { chunk, path };
